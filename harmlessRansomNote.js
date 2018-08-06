715b3a48729fc88fe4b77bae5d92781310de194c
efmcoder <faithefmcoder@gmail.com>
2018-08-06 00:43:42 -0400

    cplus

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -17,3 +17,4 @@ harmlessRansomNote(" ", "this is all the magazine text in the magazine");
 
 //Answer when you call: node harmlessRansomNote.js :
 //{this: 1, is: 1, all: 1, the: 2, magazine: 2, text:1, in:1}
+//All the codes now work
