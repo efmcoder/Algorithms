13419f7290004997fd7501b0518b84b3729eac75
efmcoder <faithefmcoder@gmail.com>
2018-08-08 23:57:57 -0400

    Anagram

---
+++ anagram.js
@@ -0,0 +1,11 @@
+function isAnagram(wordone, wordtwo){
+
+  var wordArr = string.split(" ");
+  var validCharacters = "abcdefghijklmnopqrstuvwxyz".split(" ");
+
+
+  wordArr.forEach(char => {
+    if validCharacters.indexOf(char) > -1)
+  })
+
+}
