a3b95be22c609b04aaef72becfc4b840cc1424f9
efmcoder <faithefmcoder@gmail.com>
2018-11-25 23:39:42 -0500

    anagram

--- anagram.js
+++ anagram.js
@@ -1,30 +1,43 @@
 function isAnagram(text, note)
 {
-  var textArr = text.split("");
-  var noteArr = note.split("");
+  var textArr = text.split(" ");
+  var noteArr = note.split(" ");
   //var textObj = {};
   var textObj = {};
   var noteObj = {};
+  var validCharacters = "abcdefghijklmnopqrstuvwxyz".split(" ");
 
-  textArr.forEach(function(word){
+  /*textArr.forEach(function(word){
     if(!textObj[word]) textObj[word] = 0;
     textObj[word]++;
 });
   noteArr.forEach(function(word){
     if(!noteObj[word]) noteObj[word] = 0;
     noteObj[word]++;
+});*/
+
+textArr.forEach(function(word){
+  if(validCharacters.indexOf(char) > -1) textArr.push(char);
 });
 
-var Anagram = true;
+noteArr.forEach(function(word){
+  if(validCharacters.indexOf(char) > -1) noteArr.push(char);
+});
+
+//var Anagram = true;
+if(textArr.join("") === noteArr.join("")) /*return true;
+else return false;
+}
+
 textArr.forEach(function(word){
   if(noteObj[word]){
     noteObj[word]--;
-    if(noteObj[word]<0) Anagram = false;
-  }
+    if(noteObj[word]<0) */Anagram = true;
+/*  }
   else Anagram = false;
-});
+});*/
 return Anagram;
 
-  console.log(textObj,noteObj);
+//  console.log(textObj,noteObj);
 }
-console.log(isAnagram("dog", "cat"));
+console.log(isAnagram("teacher", "cheater"));
