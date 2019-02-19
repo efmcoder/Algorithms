25ec99c47f651c4a77f06e8c5875c1a05204e0c6
efmcoder <faithefmcoder@gmail.com>
2019-02-18 20:58:59 -0500

    largest number in array

--- largestNumberInArray.js
+++ largestNumberInArray.js
@@ -1,21 +1,4 @@
 console.log(Math.max(4,5,1,3));
-
-function largestOfFour(arr){
-  //for (var i = 0; i < arr.length; i++) {
-    //arr[i]
-    var results = [];
-    for (var n = 0; n < arr.length; n++) {
-      var largestNumber = arr[n][0];
-    for (var sb = 1; sb < arr[n].length; sb++) {
-      if (arr[n][sb] > largestNumber) {
-        largestNumber = arr[n][sb];
-      }
-    }
-    results[n] = largestNumber;
-    }
-
-  return results;
-  //return arr;
-}
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26],
-  [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+//the above doesn't work for arrays, so see below
+var num  = [4,5,1,3];
+console.log(Math.max.apply(null, num));
