82e86fe782e3b44ffd038b49f74bf68915e831bd
efmcoder <faithefmcoder@gmail.com>
2019-03-03 20:20:36 -0500

    objects & methods

--- objects.js
+++ objects.js
@@ -5,5 +5,27 @@ let duck = {
     return "The name of the duck is " + duck.name;
   }
 }
-console.log(duck);
 console.log(duck.sayName());
+
+
+let residence = {
+  state: "Rhode Island",
+  city: "Cranston",
+  village: "Pawtuxet",
+  neighborhood: "Edgewood",
+  sayResidence: function(){
+    return "I reside in the state of " + residence.state + " in the city of " + residence.city
+    + ", the village of " +  residence.village + " in the neighborhood of " + residence.neighborhood;
+  }
+}
+console.log(residence.sayResidence());
+
+
+let school = {
+  schoolName: "Princeton",
+  location: "New Jersey",
+  stateSchool: function(){
+    return "I go to " + this.schoolName + " University."; //this keyword refers to the object in which schoolName appears
+  }
+}
+console.log(school.stateSchool());
