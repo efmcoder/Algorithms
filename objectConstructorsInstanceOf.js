718aea1bebb97689c35e4f9f8acaf0b7a63995b8
efmcoder <faithefmcoder@gmail.com>
2019-03-03 22:19:46 -0500

    Object Constructor instanceOf

--- objectConstructorsInstanceOf.js
+++ objectConstructorsInstanceOf.js
@@ -1,6 +1,6 @@
 //When a constructor function creates a new object, that new object is said to be an INSTANCE OF its constructor.
 //instanceof() checks for this
-let Bird = function(name, color){
+let Bird = function(name, color, numLegs){
   this.name = name;
   this.color = color;
   this.numLegs = numLegs;
@@ -8,3 +8,14 @@ let Bird = function(name, color){
 
 let crow = new Bird("Alexis", "black", 3);
 console.log(crow);
+console.log(crow instanceof Bird); //Answer = true
+
+
+//FREECODECAMP EXAMPLE:
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+let myHouse = new House(3);
+console.log(myHouse instanceof House);
