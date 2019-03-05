28555f4f135760f5c74bbfacbbb81f491449ccd9
efmcoder <faithefmcoder@gmail.com>
2019-03-04 22:44:14 -0500

    IIFE Immediately Invoked Function Expressions

---
+++ immediatelyInvokedFunctionExpressionIIFE.js
@@ -0,0 +1,12 @@
+//TRADITIONAL CALL FUNCTION
+function makeNest() {
+  console.log("A cozy nest is ready");
+}
+
+makeNest();
+
+
+//IIFE FUNCTION
+(function () {
+  console.log("A cozy nest is ready here on this IIFE too!");
+})();
